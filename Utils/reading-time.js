a0ae0dd459810017d3edb2416132913c7a4ea170
Toba <tobajamezmadamori@gmail.com>
2022-10-26 18:35:14 +0100

    added readint-time utility

---
+++ Utils/reading-time.js
@@ -0,0 +1,22 @@
+const { InternalServerError } = require('../Errors')
+const logger = require('./logger')
+
+const readingTime = (body) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const wordCount = body.split(' ').length
+            let count = Number(wordCount / 200)
+            if (count < 1) count = 1
+
+            const minutes = Math.round(count).toString() + ' ' + 'minutes'
+            resolve(minutes)
+        } catch (error) {
+            logger.error(error.message)
+            reject(new InternalServerError('something went wrong, please try again later.'))
+        }
+    })
+}
+
+module.exports = {
+    readingTime
+}
