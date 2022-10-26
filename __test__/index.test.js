0622d6e8cb2802651cd16177c9770ee77ce4586a
Toba <tobajamezmadamori@gmail.com>
2022-10-26 17:13:58 +0100

    added test case for the status endpoint

---
+++ __test__/index.test.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+const request = require('supertest')
+const app = require('../app')
+
+describe('App Instantiation', () => {
+    test('Should show a documentation link', async () => {
+        const response = await request(app)
+            .get('/')
+
+        expect(response.text).toBe('<h4>exam api is up and running at </h4><a href="">Documentation</a>')
+    })
+})
